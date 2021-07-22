4638f2a43abaeb0078f5e72b6bfd93a902506936
rolikasi <bayizbekovedil@gmail.com>
2021-07-22 16:31:54 +0300

    add-links_fix-arrows_refactor_add-tests

---
+++ components/footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <a target="_blank" href="https://github.com/Rolikasi/digital-public-goods-map">
+        Contribute
+      </a>
+      <span>
+        Data:{" "}
+        <a
+          target="_blank"
+          href="https://docs.google.com/spreadsheets/d/1t75gYVhdUjPD1532DbPYN49FLXFhpRwEBFiS4Hbk6_Q/edit#gid=728344896"
+        >
+          Pathfinders and Story
+        </a>{" "}
+        |{" "}
+        <a target="_blank" href="https://github.com/unicef/publicgoods-candidates">
+          DPGs
+        </a>
+      </span>
+    </div>
+  );
+}
